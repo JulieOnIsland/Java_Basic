9c487a69476bb8d7efd4c4da30880989d8e6fb2d
Juhyun Lee <91438461+JulieOnIsland@users.noreply.github.com>
2023-08-28 00:00:35 +0900

    Add files via upload

---
+++ BinaryTree/CompleteBinaryTest.java
@@ -0,0 +1,26 @@
+package live.algorithm.basic;
+
+public class CompleteBinaryTest {
+ 
+	public static void main(String[] args) {
+		
+		int size = 9;
+		CompleteBinaryTree<Character> tree = new CompleteBinaryTree<>(size);
+		 
+		for (int i = 0; i < size; i++) {
+			tree.add((char)(65+i));
+		}
+		 
+//		tree.bfs3();
+		tree.dfsByPreOrder(1);
+		System.out.println();
+//		tree.dfsByInOrder(1);
+//		System.out.println();
+//		tree.dfsByPostOrder(1);
+//		System.out.println();
+		tree.dfs();
+ 
+	}
+
+}
+  
